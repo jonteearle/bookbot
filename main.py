16d594490d0dab60a24d352e98a8d19d34e4fcd9
jonteearle <jonteearle@icloud.com>
2025-12-31 00:38:24 +1000

    add report

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-from stats import get_num_words, get_num_chars
+from stats import get_num_words, get_num_chars, sort_num_chars
 
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     num_chars = get_num_chars(text)
+    num_chars_sorted = sort_num_chars(num_chars)
+
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
-    print(num_chars)
+    print("--------- Character Count -------")
+    for dict in num_chars_sorted:
+        if dict["char"].isalpha() == True:
+            print(f"{dict["char"]} {dict["num"]}")
+    print("============= END ===============")
 
 def get_book_text(path):
     with open(path) as f:
--- stats.py
+++ stats.py
@@ -13,3 +13,16 @@ def get_num_chars(text):
         else:
             chars[char] += 1
     return chars
+
+def sort_on(items):
+    return items["num"]
+
+def sort_num_chars(dict):
+    dict_list = []
+    for char in dict:
+        new_dict = {}
+        new_dict["char"] = char
+        new_dict["num"] = dict[char]
+        dict_list.append(new_dict)
+    sorted_list = dict_list.sort(reverse=True, key=sort_on)
+    return dict_list
