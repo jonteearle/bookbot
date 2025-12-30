54448117080246c99ace64663defb94dbd97f868
jonteearle <jonteearle@icloud.com>
2025-12-31 01:04:42 +1000

    final checks

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
+import sys
 from stats import get_num_words, get_num_chars, sort_num_chars
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     num_chars = get_num_chars(text)
     num_chars_sorted = sort_num_chars(num_chars)
 
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {sys.argv[1]}...")
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
     for dict in num_chars_sorted:
         if dict["char"].isalpha() == True:
-            print(f"{dict["char"]} {dict["num"]}")
+            print(f"{dict["char"]}: {dict["num"]}")
     print("============= END ===============")
 
 def get_book_text(path):
